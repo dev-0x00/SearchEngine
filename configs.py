3da184a62604b5053c5ba766af29e79800ed8fac
lilsidoh <asidoihsidney254@gmail.com>
2022-05-31 16:13:57 +0300

    chromedriver configurations

--- configs.py
+++ configs.py
@@ -34,25 +34,22 @@ class Config:
             with zipfile.ZipFile(pluginFile, "w") as zipFile:
                 zipFile.writestr("manifest.json", self.manifest_json)
                 zipFile.writestr("background.js", self.background_js)
-            #chromeOptions.add_extension(pluginFile)
+           
 
         if user_agent:
             chromeOptions.add_argument("--user-agent=%s" % user_agent)
         
-        option = uc.ChromeOptions()
         option = webdriver.ChromeOptions()
         chrome_prefs = {}
-        #option.experimental_options["prefs"] = chrome_prefs
         chrome_prefs["profile.default_content_settings"] = {"images": 2}
         chrome_prefs["profile.managed_default_content_settings"] = {"images": 2} 
-        service = Service("./chromedriver")
-        #option.add_argument("--proxy-server=http://p.webshare.io:9999")
+        option.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
         option.add_argument("--incognito")
         option.add_experimental_option("excludeSwitches", ["enable-automation"])
         chromeOptions.add_experimental_option('useAutomationExtension', False)
         option.add_argument("--headless")
         chromeOptions.add_argument("--disable-blink-features=AutomationControlled")
-        driver = webdriver.Chrome(service=service, options=option)
+        driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=option)
         return driver
 
 if __name__ == "__main__":
